ad150be999282fb8bd9a7258b2af4606f3ad8085
norplys <joywinata88@gmail.com>
2023-08-10 15:41:19 +0700

    adding the game logic

--- src/App.tsx
+++ src/App.tsx
@@ -11,16 +11,55 @@ const [isx, setIsx] = useState<boolean>(false);
 
 function handleClick(event : number) {
   let copyarray = [...array];
+  if (copyarray[event] !== null || countingWinner(array)){
+    return;
+  }
+  if(isx === true){
   copyarray[event] = 'x';
+  setIsx(!isx); 
   setArray(copyarray);
+  return;};
+
+  copyarray[event] = 'O';
+  setIsx(!isx);
+  setArray(copyarray);
+};
+
+let winners = countingWinner(array);
+let stat  = ``;
+if(winners){
+  stat = `Winner = ${winners}`
+}else{
+  stat = `Turn = ${isx ? 'X' : 'O'}`
 }
+
 
   return (
     <>
       <div className='history'></div>
-      <Board click = {handleClick} array = {array}/>
+      <Board  click = {handleClick} array = {array} stat= {stat}/>
     </>
   )
+}
+
+function countingWinner (array : (null | string)[]) : string | null | boolean | void {
+   const winner : number[][] = [
+      [0,1,2],
+      [0,3,6],
+      [0,4,8],
+      [1,4,7],
+      [2,4,6],
+      [2,5,8],
+      [3,4,5],
+      [6,7,8]
+   ]
+  for(let i= 0 ; i < winner.length; i++){
+      const[a,b,c] = winner[i];
+      if(array[a] && array[a] === array[b] && array[c]){
+        return array[a];
+      }
+  }
+  return false;
 }
 
 export default App
--- src/assets/component/board.tsx
+++ src/assets/component/board.tsx
@@ -3,18 +3,19 @@ type boardProps = {
     click : (event : number) => void,
     square? : string | null,
     array : (null|string)[],
+    stat : string,
 }
-type mapping = {
-    each : null | string,
-    i : number,
-}
+
 
 export default function Board(props : boardProps){
     const buttons = [...props.array];
     
     return(
+    <div className="outside">
+        {props.stat}
     <div className="board">
-        {buttons.map((each , i)   => <Button numValue = {i} value = {i.toString()} key = {i} click = {props.click} square = {each}/>)}
+        {buttons.map((each, i)   => <Button numValue = {i} value = {i.toString()} key = {i} click = {props.click} square = {each}/>)}
+    </div>
     </div>
     );
 }
